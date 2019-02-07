3dfb8e2015c5457e9be36d811fe07c9b39d49047
nassim <nassim.mouline@gmail>
2019-02-07 22:11:14 +0100

    admin role supervising editing and deleting

--- src/Controllers/ChatterPostController.php
+++ src/Controllers/ChatterPostController.php
@@ -171,7 +171,7 @@ class ChatterPostController extends Controller
         }
 
         $post = Models::post()->find($id);
-        if (Sentinel::check() && (Sentinel::getUser()->id == $post->user_id)) {
+        if (Sentinel::check() && (Sentinel::getUser()->id == $post->user_id || Sentinel::getUser()->roles()->first()->slug == 'admin') ) {
             if ($post->markdown) {
                 $post->body = $request->body;
             } else {
@@ -214,7 +214,7 @@ class ChatterPostController extends Controller
     {
         $post = Models::post()->with('discussion')->findOrFail($id);
 
-        if ($request->user()->id !== (int) $post->user_id) {
+        if ($request->user()->id !== (int) $post->user_id || Sentinel::getUser()->roles()->first()->slug == 'admin') {
             return redirect('/'.config('chatter.routes.home'))->with([
                 'chatter_alert_type' => 'danger',
                 'chatter_alert'      => trans('chatter::alert.danger.reason.destroy_post'),
